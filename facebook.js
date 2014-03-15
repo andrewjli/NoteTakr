de401d52bdd9451ad85a053f5e64ab0b8b907e00
Nianu <nianu@Hunain-Ubuntu.(none)>
2014-03-15 16:56:11 +0000

    Added fb authentication

--- facebook.js
+++ facebook.js
@@ -1,3 +1,108 @@
 passport = require("passport");
 LocalStrategy = require('passport-local').Strategy;
 FacebookStrategy = require('passport-facebook').Strategy;
+
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.session({ secret: 'SECRET' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost/myapp");
+
+var LocalUserSchema = new mongoose.Schema({
+username: String,
+salt: String,
+hash: String
+});
+
+var Users = mongoose.model('userauths', localUserSchema);
+
+var FacebookUserSchema = new mongoose.Schema({
+    fbId: String,
+    email: { type : String , lowercase : true},
+    name : String
+});
+
+var FbUsers = mongoose.model('fbs',FacebookUserSchema);
+
+passport.use(new LocalStrategy(function(username, password,done){
+    Users.findOne({ username : username},function(err,user){
+        if(err) { return done(err); }
+        if(!user){
+            return done(null, false, { message: 'Incorrect username.' });
+        }
+
+        hash( password, user.salt, function (err, hash) {
+            if (err) { return done(err); }
+            if (hash == user.hash) return done(null, user);
+            done(null, false, { message: 'Incorrect password.' });
+        });
+    });
+}));
+
+passport.use(new FacebookStrategy({
+    clientID: "1471471923068168",
+    clientSecret: "2e3b7ed934f856c7cbd33d643d3d1e1c",
+    //Running on localhost- change this later
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    FbUsers.findOne({fbId : profile.id}, function(err, oldUser){
+        if(oldUser){
+            done(null,oldUser);
+        }else{
+            var newUser = new FbUsers({
+                fbId : profile.id ,
+                email : profile.emails[0].value,
+                name : profile.displayName
+            }).save(function(err,newUser){
+                if(err) throw err;
+                done(null, newUser);
+            });
+        }
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+
+passport.deserializeUser(function(id, done) {
+    FbUsers.findById(id,function(err,user){
+        if(err) done(err);
+        if(user){
+            done(null,user);
+        }else{
+            Users.findById(id, function(err,user){
+                if(err) done(err);
+                done(null,user);
+            });
+        }
+    });
+});
+
+//Following functions chekc authentication and if logged in
+
+function authenticatedOrNot(req, res, next){
+    if(req.isAuthenticated()){
+        next();
+    }else{
+        res.redirect("/login");
+    }
+}
+
+function userExist(req, res, next) {
+    Users.count({
+        username: req.body.username
+    }, function (err, count) {
+        if (count === 0) {
+            next();
+        } else {
+            // req.session.error = "User Exist"
+            res.redirect("/singup");
+        }
+    });
+}
